c1abf79541fe84d7f8829f27cc0ef8d363563d63
Navdeep K <83499821+crypto-navdeep@users.noreply.github.com>
2021-05-23 17:17:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-#
-print("Hello World !")
+# Imports
+import webbrowser
+from time import sleep
+
+print("""
+\n
+ ██████╗  ██████╗  ██████╗  ██████╗ ██╗     ███████╗
+██╔════╝ ██╔═══██╗██╔═══██╗██╔════╝ ██║     ██╔════╝
+██║  ███╗██║   ██║██║   ██║██║  ███╗██║     █████╗  
+██║   ██║██║   ██║██║   ██║██║   ██║██║     ██╔══╝  
+╚██████╔╝╚██████╔╝╚██████╔╝╚██████╔╝███████╗███████╗
+ ╚═════╝  ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+\n                                            
+""")
+# Execute
+while True:
+    query = input("Enter your query :\n")
+    url = (f"https://www.google.com/search?q={query}")
+    webbrowser.open(url)
+
+
+# End
